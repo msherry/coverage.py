f4097c6000d4cdb95e1657082df3eaf8f2aa87d1
bninja <andrew@poundpay.com>
2014-01-08 16:14:33 -0800

    releasable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='coverage.py',
+    version='0.1',
+    url='http://github.com/msherry/coverage.py',
+    description='',
+    long_description='',
+    scripts=['coverage.py'],
+    install_requires=['lxml'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
